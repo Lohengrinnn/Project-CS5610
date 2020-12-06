6c9f931064e2c1ec3f8e162cf2000005d0378aeb
Lin Cao <lincao.auburn@gmail.com>
2020-12-06 14:04:03 -0800

    date

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from "@angular/common/http";
 
-//const productUrl = 'http://localhost:8080/api/products';
-const productUrl = "https://server-node-js-jiongwu.herokuapp.com/api/products"
+const productUrl = 'http://localhost:8080/api/products';
+//const productUrl = "https://server-node-js-jiongwu.herokuapp.com/api/products"
 
 @Injectable({
   providedIn: 'root',
