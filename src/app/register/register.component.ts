291dedb2172461ff9e4e2d3ba394eb9a92fa15b2
Jiong Wu <lohengrin1024@gmail.com>
2020-12-14 11:36:06 -0800

    refresh when registering

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {UserService} from "../../services/user.service";
+import {AppComponent} from "../app.component";
 
 @Component({
   selector: 'app-register',
@@ -12,14 +13,18 @@ export class RegisterComponent implements OnInit {
   password: String = '';
   password_verify: string = '';
   constructor(private router: Router,
-              private usersService: UserService) { }
+              private usersService: UserService,
+              private appComp: AppComponent) { }
 
   register = () => {
     if (this.password !== this.password_verify) {
       alert("<password> is different from <verify password>")
     } else {
       this.usersService.register(this.username, this.password)
-        .then(actualUser => this.router.navigate(['profile']));
+        .then(actualUser => {
+          this.appComp.ngOnInit();
+          this.router.navigate(['profile']);
+        });
     }
   }
 
